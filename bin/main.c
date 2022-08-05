aedd3e6ef7a478dab0e392eb46ea6004fb2a53e5
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2022-08-04 23:59:58 -0300

    Add `add` and `sub` pkgs, drop `math`

--- bin/main.c
+++ bin/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-#include "lib/math/math.h"
+#include "lib/math/add.h"
+#include "lib/math/sub.h"
 
 int main() {
     printf("1 + 2 = %d\n", add(1, 2));
+    printf("2 - 1 = %d\n", sub(2, 1));
     return 0;
 }
